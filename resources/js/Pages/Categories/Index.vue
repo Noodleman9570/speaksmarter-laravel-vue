8343d7212ffbd9449423c03a504b733255689730
Kevin <kevinsaavedra55@gmail.com>
2024-03-09 13:23:52 -0400

    Su puta madre

--- resources/js/Pages/Categories/Index.vue
+++ resources/js/Pages/Categories/Index.vue
@@ -7,6 +7,19 @@ export default {
 <script setup>
 import AppLayout from "@/Layouts/AppLayout.vue";
 import { Link } from "@inertiajs/vue3";
+
+defineProps({
+    categories: {
+        type: Object,
+        required: true
+    }
+})
+
+const deleteCategory = id => {
+    if (confirm('Are you sure?')) {
+        Inertia.delete(route('categories.destroy', id))
+    }
+}
 </script>
 
 <template>
@@ -24,18 +37,19 @@ import { Link } from "@inertiajs/vue3";
                         <div class="flex justify-between">
                             <Link :href="route('categories.create')" class="text-white bg-indigo-500 hover:bg-indigo-700 rounded py-2 px-4">
                                 CREATE CATEGORY
+                                {{ categories }}
                             </Link>
                         </div>
                     </div>
                     <div class="mt-4">
                         <ul role="list" class="divide-y divide-gray-100">
-                            <li class="flex justify-between gap-x-6 py-5">
+                            <li class="flex justify-between gap-x-6 py-5" v-for="category in categories.data">
                                 <div class="flex min-w-0 gap-x-4">
                                     <div class="min-w-0 flex-auto">
                                         <p
                                             class="text-md font-semibold leading-6 text-gray-900"
                                         >
-                                            Leslie Alexander
+                                            {{ category.name }}
                                         </p>
                                     </div>
                                 </div>
@@ -43,28 +57,11 @@ import { Link } from "@inertiajs/vue3";
                                     class="hidden shrink-0 sm:flex sm:flex-col sm:items-end"
                                 >
                                     <p class="text-md leading-6 text-gray-900">
-                                        Edit Delete
+                                        <Link :href="route('categories.edit', category.id)">Edit</Link> <Link @click="deleteCategory(category.id)">Eliminar</Link>
                                     </p>
                                 </div>
                             </li>
-                            <li class="flex justify-between gap-x-6 py-5">
-                                <div class="flex min-w-0 gap-x-4">
-                                    <div class="min-w-0 flex-auto">
-                                        <p
-                                            class="text-md font-semibold leading-6 text-gray-900"
-                                        >
-                                            Leslie Alexander
-                                        </p>
-                                    </div>
-                                </div>
-                                <div
-                                    class="hidden shrink-0 sm:flex sm:flex-col sm:items-end"
-                                >
-                                    <p class="text-md leading-6 text-gray-900">
-                                        Edit Delete
-                                    </p>
-                                </div>
-                            </li>
+
                         </ul>
                     </div>
                 </div>
